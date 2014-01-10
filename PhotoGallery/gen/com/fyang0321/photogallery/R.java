74cdce36a63207c0e31961238b0f2b1d860ef138
fyang0321 <fyang880321@gmail.com>
2014-01-09 23:03:04 -0600

    create fragment, activity, fetchr

--- PhotoGallery/gen/com/fyang0321/photogallery/R.java
+++ PhotoGallery/gen/com/fyang0321/photogallery/R.java
@@ -24,10 +24,13 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f080000;
+        public static final int action_settings=0x7f080002;
+        public static final int fragmentContainer=0x7f080000;
+        public static final int gridView=0x7f080001;
     }
     public static final class layout {
-        public static final int activity_photo_gallery=0x7f030000;
+        public static final int activity_fragment=0x7f030000;
+        public static final int fragment_photo_gallery=0x7f030001;
     }
     public static final class menu {
         public static final int photo_gallery=0x7f070000;
--- PhotoGallery/src/com/fyang0321/photogallery/PhotoGalleryActivity.java
+++ PhotoGallery/src/com/fyang0321/photogallery/PhotoGalleryActivity.java
@@ -1,22 +1,12 @@
 package com.fyang0321.photogallery;
 
-import android.os.Bundle;
-import android.app.Activity;
-import android.view.Menu;
+import android.support.v4.app.Fragment;
 
-public class PhotoGalleryActivity extends Activity {
+public class PhotoGalleryActivity extends SingleFragmentActivity {
 
 	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_photo_gallery);
-	}
-
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.photo_gallery, menu);
-		return true;
+	public Fragment createFragment() {
+		return new PhotoGalleryFragment();
 	}
 
 }
